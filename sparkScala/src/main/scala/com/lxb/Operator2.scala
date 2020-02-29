8fefb0894c4c6bf8772097270f5fbf20f9147300
lxb130 <lxb1304@163.com>
2020-02-29 21:40:26 +0800

    行动算子

---
+++ sparkScala/src/main/scala/com/lxb/Operator2.scala
@@ -0,0 +1,25 @@
+package com.lxb
+
+import com.lxb.Operator1.sc
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Operator2 {
+
+  var conf = new SparkConf()
+  conf.setAppName("key-value型算子练习")
+  conf.setMaster("local[*]")
+  var sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+    CollectAsMap()
+    sc.stop()
+  }
+
+  def CollectAsMap(): Unit = {
+    val first = sc.parallelize(List(("张一", 1), ("李二", 1), ("张一", 2), ("李二", 2), ("张一", 3), ("李二", 3)), 2)
+    var second = first.collectAsMap()
+    println(second.toMap)
+  }
+
+
+}
