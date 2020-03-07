27ceca059c27906d6287e81c7937970c9fffe929
lxb130 <lxb1304@163.com>
2020-03-07 10:14:07 +0800

    一个for循环实现九九乘法表

---
+++ javastudy/src/com/lxb/ch15/NineNineMulitTable.java
@@ -0,0 +1,17 @@
+package com.lxb.ch15;
+
+public class NineNineMulitTable {
+
+	public static void main(String[] args) {
+		for (int i = 1, j = 1; j <= 9; i++) {
+			System.out.print(i+"*"+j+"="+i*j+"\t");
+			if(i==j){
+				i=0;
+				j++;
+				System.out.println();
+			}
+		}
+
+	}
+
+}
