a5e7972918b7cbe3f76071348cfe578a2c6d6176
lxb130 <lxb1304@163.com>
2020-03-07 10:18:42 +0800

    核心思想  一行一行实现

--- javastudy/src/com/lxb/ch15/NineNineMulitTable.java
+++ javastudy/src/com/lxb/ch15/NineNineMulitTable.java
@@ -2,6 +2,8 @@ package com.lxb.ch15;
 
 public class NineNineMulitTable {
 
+	//核心思想：一行一行实现
+	
 	public static void main(String[] args) {
 		for (int i = 1, j = 1; j <= 9; i++) {
 			System.out.print(i+"*"+j+"="+i*j+"\t");
